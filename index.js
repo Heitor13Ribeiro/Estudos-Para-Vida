e0245c596fed6b95c8edf9175f7fef0239b4bf4c
Heitor13Ribeiro <66147239+Heitor13Ribeiro@users.noreply.github.com>
2020-05-29 21:43:35 -0300

    Foi feito o upload

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
-const app = express();
+const app = express(); 
+
+app.get('/', (Request, Response)=>{
+    return Response.send('Hello World')
+});
 
 app.listen(3333);
+
